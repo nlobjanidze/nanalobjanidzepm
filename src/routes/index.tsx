bfd108ff59554c1406b7b5d8d8430f7c935a222c
nlobjanidze <nlobjan21@gmail.com>
2026-09-01 18:17:11 +0400

    Refactor Index component to remove LanguageContext
    Removed LanguageContext and related provider functions from the Index component.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,32 +1,42 @@
-import { createContext, useContext, useState, type ReactNode } from "react";
+import { createFileRoute } from "@tanstack/react-router";
+import { SiteNav } from "@/components/site/SiteNav";
+import { Hero } from "@/components/site/Hero";
+import { About } from "@/components/site/About";
+import { Certifications } from "@/components/site/Certifications";
+import { Services } from "@/components/site/Services";
+import { Trainings } from "@/components/site/Trainings";
+import { Experience } from "@/components/site/Experience";
+import { Projects } from "@/components/site/Projects";
+import { LeadershipRecognition } from "@/components/site/LeadershipRecognition";
+import { GlobalEngagement } from "@/components/site/GlobalEngagement";
 
-export type Language = "ka" | "en";
+import { Testimonials } from "@/components/site/Testimonials";
+import { ContactForm } from "@/components/site/ContactForm";
+import { SiteFooter } from "@/components/site/SiteFooter";
 
-type LanguageContextType = {
-  language: Language;
-  setLanguage: (language: Language) => void;
-};
+export const Route = createFileRoute("/")({
+  component: Index,
+});
 
-const LanguageContext = createContext<LanguageContextType | undefined>(
-  undefined
-);
-
-export function LanguageProvider({ children }: { children: ReactNode }) {
-  const [language, setLanguage] = useState<Language>("ka");
-
+function Index() {
   return (
-    <LanguageContext.Provider value={{ language, setLanguage }}>
-      {children}
-    </LanguageContext.Provider>
+    <div className="min-h-screen bg-background text-foreground">
+      <SiteNav />
+      <main>
+        <Hero />
+        <About />
+        <Services />
+        <Trainings />
+        <Experience />
+        <Projects />
+        <LeadershipRecognition />
+        <GlobalEngagement />
+
+        <Certifications />
+        <Testimonials />
+        <ContactForm />
+      </main>
+      <SiteFooter />
+    </div>
   );
-}
-
-export function useLanguage() {
-  const context = useContext(LanguageContext);
-
-  if (!context) {
-    throw new Error("useLanguage must be used inside LanguageProvider");
-  }
-
-  return context;
 }
