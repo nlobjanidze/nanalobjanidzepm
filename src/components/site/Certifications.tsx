7c56cbbcd355a4075aadc2f54b64e4fe6c8351ea
nlobjanidze <nlobjan21@gmail.com>
2026-09-02 13:33:18 +0400

    Change grid alignment in Certifications component

--- src/components/site/Certifications.tsx
+++ src/components/site/Certifications.tsx
@@ -128,7 +128,7 @@ export function Certifications() {
         </div>
 
         {/* Certifications grid */}
-        <div className="mt-8 grid lg:grid-cols-2 gap-6 items-stretch">
+        className="mt-8 grid lg:grid-cols-2 gap-6 items-start"
           <Group title="PMI სერტიფიკატები" items={PMI} />
           <Group title="საერთაშორისო პროფესიული სერტიფიკატები" items={PROFESSIONAL} />
         </div>
